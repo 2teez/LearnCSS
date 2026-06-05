10b2e9ffb6d43247a918ed04c6af34f9d97dca06
timothy adigun <adigun.timothy@gmail.com>
2026-06-05 21:58:32 +0100

    added options to makecc bashscript

--- makecss.sh
+++ makecss.sh
@@ -17,12 +17,84 @@ echo "  -p: Create a full flage project directory with seperate folders for"
 echo "      css, js and an index.html or index.php file"
 }
 
-HTMLFILE="
+# global filename
+filename="main"
 
+HTMLFILE="
+<!DOCTYPE html>
+<html lang=\"en\">
+    <head>
+    <!-- Required meta tags always come first -->
+    <meta charset=\"utf-8\" />
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\" />
+        <title>CSS Projects</title>
+
+        <!-- CSS -->
+        <link
+            href=\"${filename##.*}.css\" text=\"text/css\" rel=\"stylesheet\"
+        >
+    </head>
+    <body>
+        <?php
+            echo \"Hello, World\n\";
+        ?>
+
+        <!-- JS. -->
+        <script src=""></script>
+    </body>
+</html>
 "
 
 CSSFILE="
+*{
 
+}
 "
 
 [[ "${#}" -ne 2 ]] && { help_file; exit; }
+
+optionstrings="d:g:p:h"
+
+while getopts ${optionstrings} opt; do
+    case ${opt} in
+        d)
+            echo "Deleting ${OPTARG}..."
+            filename=$(basename "${OPTARG}")
+            while read -r -p "Are you sure you want to delete ${filename}? (y/n): " answer; do
+                case ${answer} in
+                    y)
+                        [[ -d "${filename}" ]] && { rm -rf "$filename"; break; }
+                        rm -f "${filename}"
+                        break
+                        ;;
+                    n)
+                        echo "Deletion cancelled."
+                        break
+                        ;;
+                    *)
+                        echo "Invalid input. Please enter 'y' or 'n'."
+                        ;;
+                esac
+            done
+            ;;
+        g)
+            echo "Creating generic css file with html file with default content..."
+            filename="${OPTARG}"
+            echo "${CSSFILE}" > "${filename##.*}.css"
+            echo "${HTMLFILE}" > "${filename##.*}.php"
+            ;;
+        p)
+            echo "Creating project ${OPTARG}..."
+            filename=$(basename "${OPTARG}")
+            mkdir -p "${filename}" "${filename}/css" "${filename}/js"
+
+            touch "${filename}/js/main.js"
+            echo "${CSSFILE}" > "${filename}/css/main.css"
+            echo "${HTMLFILE}" > "${filename}/index.php"
+            ;;
+        *)
+            echo "Invalid option: -${OPTARG}" >&2
+            exit 1
+            ;;
+    esac
+done
